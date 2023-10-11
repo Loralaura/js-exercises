8862ca11af626725c4ff482b6f6afe66e7910eef
Laura Robinson <laura3918@hotmail.co.uk>
2023-10-11 22:40:40 +0100

    feat: Exercise 2: checkIngredients now checks for ingredients

--- challenges/exercise2.js
+++ challenges/exercise2.js
@@ -1,10 +1,12 @@
 export function getSquares(nums) {
   if (nums === undefined) throw new Error("nums is required");
+
   return nums.map((num) => num ** 2);
 }
 
 export function camelCaseWords(words) {
   if (words === undefined) throw new Error("words is required");
+
   for (let i = 1; i < words.length; i++) {
     words[i] = words[i][0].toUpperCase() + words[i].substring(1);
   }
@@ -13,8 +15,8 @@ export function camelCaseWords(words) {
 
 export function getTotalSubjects(people) {
   if (people === undefined) throw new Error("people is required");
-  let total = 0;
 
+  let total = 0;
   for (let i = 0; i < people.length; i++) {
     total += people[i].subjects.length;
   }
@@ -24,7 +26,14 @@ export function getTotalSubjects(people) {
 export function checkIngredients(menu, ingredient) {
   if (menu === undefined) throw new Error("menu is required");
   if (!ingredient) throw new Error("ingredient is required");
-  // Your code here!
+
+  let hasIngredient = false;
+  for (let i = 0; i < menu.length; i++) {
+    if (menu[i].ingredients.includes(ingredient)) {
+      hasIngredient = true;
+    }
+  }
+  return hasIngredient;
 }
 
 export function duplicateNumbers(arr1, arr2) {
