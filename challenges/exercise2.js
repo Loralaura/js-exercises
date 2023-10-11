e76e4eca176398ac8b794afc93a0ba6692081612
Laura Robinson <laura3918@hotmail.co.uk>
2023-10-11 22:50:06 +0100

    feat: Exercise 2: duplicateNumbers returns duplicates in ascending order

--- challenges/exercise2.js
+++ challenges/exercise2.js
@@ -17,6 +17,7 @@ export function getTotalSubjects(people) {
   if (people === undefined) throw new Error("people is required");
 
   let total = 0;
+
   for (let i = 0; i < people.length; i++) {
     total += people[i].subjects.length;
   }
@@ -28,6 +29,7 @@ export function checkIngredients(menu, ingredient) {
   if (!ingredient) throw new Error("ingredient is required");
 
   let hasIngredient = false;
+
   for (let i = 0; i < menu.length; i++) {
     if (menu[i].ingredients.includes(ingredient)) {
       hasIngredient = true;
@@ -39,5 +41,15 @@ export function checkIngredients(menu, ingredient) {
 export function duplicateNumbers(arr1, arr2) {
   if (arr1 === undefined) throw new Error("arr1 is required");
   if (arr2 === undefined) throw new Error("arr2 is required");
-  // Your code here!
+
+  let duplicates = [];
+
+  for (let i = 0; i < arr1.length; i++) {
+    for (let j = 0; j < arr2.length; j++) {
+      if ((arr1[i] === arr2[j]) & !duplicates.includes(arr1[i])) {
+        duplicates.push(arr1[i]);
+      }
+    }
+  }
+  return duplicates.sort((a, b) => a - b);
 }
